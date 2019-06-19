b1a1cd6cde3e39dafc419434601e24559984f512
kimchanghwan <hubaimaster@gmail.com>
2019-06-19 18:25:32 +0900

    Add Briefly-address

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# aws-interface-tuto
+# [AWS Interface](https://aws-interface.com) Tutorial
+
+This is the source code repository for the AWSI SDK Tutorial. 
+
+# How to contribute
+
+AWSI Tutorial is licensed under a [*Apache 2.0*](http://www.apache.org/licenses/LICENSE-2.0) license. Everyone is free to add, edit and correct the tutorial.
+
+The contributor must follow the directory structure below:
+
++-- README.md
++-- webjs
+|   +-- [contributer's_folder]
+|   |   +-- README.md
+|   |   +-- ... Any files
+|   +-- [contributer2's_folder]
++-- python3
+|   +--  [contributer3's_folder]
+|   +--  [contributer4's_folder]
++-- java
+|   +--  [contributer5's_folder]
+|   +--  [contributer6's_folder]
++-- nodejs
+|   +--  [contributer7's_folder]
+|   +--  [contributer8's_folder]
++-- swift
+|   +--  [contributerN's_folder]
+
+# Editing
+
+The source code of the tutorial is [hosted on GitHub](https://github.com/hubaimaster/aws-interface-tuto). The GitHub [Fork & Pull workflow](https://help.github.com/articles/using-pull-requests) is used to accept and review changes.
+
+The tutorial readme is written in [Markdown mark up language](https://help.github.com/articles/markdown-basics).
+
+You can find any discussions about the contents of the tutorial on the [GitHub issue tracker](https://github.com/aws-interface-tuto/issues).
+
+# Making a pull request
+
+After you have finished your changes you need to create [a pull request](https://help.github.com/articles/using-pull-requests)  on GitHub. AWSI developer team will get notified about the pull request, review your changes, suggest any corrections if needed and then *pull* your changes to the master version.
+
+In your own repository on GitHub press do *Compare & pull request*
+
+![Compare & pull request](contributing/images/pull_request.png)
+
+Fill in the information *why* this change is being made. The reviewer can see the details of the actual change, so you don't need repeat the content of the change.
+
+Then just press *Create pull request*.
---
+++ webjs/briefly-address/aws_interface.js
@@ -0,0 +1,308 @@
+
+class Client{
+
+    constructor() {
+        this.set_session_id(null);
+        this.set_base_url('https://ywrja3de0j.execute-api.ap-northeast-2.amazonaws.com/prod_aws_interface/iYKCogjSnKtzYXCphK4oxE');
+    }
+
+    get_base_url(){
+        return this.base_url;
+    }
+
+    set_base_url(base_url){
+        this.base_url = base_url;
+    }
+
+    get_session_id(){
+        return this.session_id;
+    }
+
+    set_session_id(session_id){
+        this.session_id = session_id;
+    }
+
+    static _get(object, key, default_value=null) {
+        var result = object[key];
+        return (typeof result !== "undefined") ? result : default_value;
+    }
+
+    _call_api(service_type, function_name, data, callback) {
+        if (data == null){
+            data = {};
+        }
+        data['module_name'] = 'cloud.' + service_type + '.' + function_name;
+        if (this.get_session_id() != null){
+            data['session_id'] = this.get_session_id();
+        }
+        this._post(this.get_base_url(), data, callback);
+    }
+
+    _post(url, data, callback){
+        var req = new XMLHttpRequest();
+        req.open("POST", url, true);
+        req.setRequestHeader("Accept", "application/json");
+        req.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+        req.onreadystatechange = function (aEvt) {
+          if (req.readyState == 4) {
+            var json = JSON.parse(req.responseText);
+            var body = null;
+            var error = null;
+            if ("body" in json){
+              body = json["body"];
+            }
+            if ("error" in json){
+              error = json["error"];
+              console.error(error);
+            }
+            callback(body);
+          }
+        };
+        req.send(JSON.stringify(data));
+    }
+
+    _auth(api_name, data, callback) {
+        let self = this;
+        this._call_api('auth', api_name, data, function (data) {
+            self.log_create_log('auth', api_name, null, function (data) {
+
+            });
+            callback(data);
+        });
+    }
+
+    _database(api_name, data, callback) {
+        let self = this;
+        this._call_api('database', api_name, data, function (data) {
+            self.log_create_log('database', api_name, null, function (data) {
+
+            });
+            callback(data);
+        });
+    }
+
+    _storage(api_name, data, callback) {
+        let self = this;
+        this._call_api('storage', api_name, data, function (data) {
+            self.log_create_log('storage', api_name, null, function (data) {
+
+            });
+            callback(data);
+        });
+    }
+
+    _logic(api_name, data, callback) {
+        this._call_api('logic', api_name, data, callback);
+    }
+
+    _log(api_name, data, callback) {
+        this._call_api('log', api_name, data, callback);
+    }
+
+    auth_register(email, password, extra={}, callback) {
+        this._auth('register', {
+            'email': email,
+            'password': password,
+            'extra': extra,
+        }, callback);
+    }
+
+    auth_login(email, password, callback) {
+        let self = this;
+        this._auth('login', {
+            'email': email,
+            'password': password,
+        }, function (data) {
+            if ('session_id' in data){
+                self.set_session_id(Client._get(data,'session_id'));
+            }
+            callback(data);
+        });
+    }
+
+    auth_get_user(user_id, callback) {
+        this._auth('get_user', {
+            'user_id': user_id,
+        }, callback);
+    }
+
+    auth_get_users(start_key=null, callback) {
+        this._auth('get_users', {
+            'start_key': start_key,
+        }, callback);
+    }
+
+    auth_logout(callback) {
+        this._auth('logout', {
+            'session_id': this.get_session_id(),
+        }, callback);
+    }
+
+    auth_guest(guest_id=null, callback) {
+        let self = this;
+        this._auth('guest', {
+            'guest_id': guest_id,
+        }, function (data) {
+            self.set_session_id(Client._get(data,'session_id'));
+            callback(data);
+        });
+    }
+
+    database_create_item(item, partition, read_groups, write_groups, callback) {
+        this._database('create_item', {
+            'item': item,
+            'partition': partition,
+            'read_groups': read_groups,
+            'write_groups': write_groups,
+        }, callback);
+    }
+
+    database_delete_item(item_id, callback) {
+        this._database('delete_item', {
+            'item_id': item_id,
+        }, callback);
+    }
+
+    database_get_item(item_id, callback) {
+        this._database('get_item', {
+            'item_id': item_id,
+        }, callback);
+    }
+
+    database_get_items(partition, start_key=null, limit=100, callback) {
+        this._database('get_items', {
+            'partition': partition,
+            'start_key': start_key,
+            'limit': limit,
+        }, callback);
+    }
+
+    database_put_item_field(item_id, field_name, field_value, callback) {
+        this._database('put_item_field', {
+            'item_id': item_id,
+            'field_name': field_name,
+            'field_value': field_value,
+        }, callback);
+    }
+
+    database_update_item(item_id, item, read_groups, write_groups, callback) {
+        this._database('update_item', {
+            'item_id': item_id,
+            'item': item,
+            'read_groups': read_groups,
+            'write_groups': write_groups,
+        }, callback);
+    }
+
+    database_query_items(partition, query, start_key=null, limit=100, reverse=false, callback) {
+        this._database('query_items', {
+            'partition': partition,
+            'query': query,
+            'start_key': start_key,
+            'limit': limit,
+            'reverse': reverse,
+        }, callback);
+    }
+
+    storage_delete_b64(file_id, callback) {
+        this._storage('delete_b64', {
+            'file_id': file_id,
+        }, callback);
+    }
+
+    storage_download_b64_chunk(file_id, callback) {
+        this._storage('download_b64', {
+            'file_id': file_id,
+        }, callback);
+    }
+
+    storage_upload_b64_chunk(parent_file_id, file_name, file_b64, read_groups, write_groups, callback) {
+        this._storage('upload_b64', {
+            'parent_file_id': parent_file_id,
+            'file_name': file_name,
+            'file_b64': file_b64,
+            'read_groups': read_groups,
+            'write_groups': write_groups,
+        }, callback);
+    }
+
+    storage_delete_file(file_id, callback) {
+        this.storage_delete_b64(file_id, callback);
+    }
+
+    storage_download_file(file_id, callback_file) {
+        let self = this;
+        var string_file_b64 = null;
+        var file_name = 'file';
+        function download(file_id, callback){
+            self.storage_download_b64_chunk(file_id, function (result) {
+                file_id = Client._get(result, 'parent_file_id', null);
+                file_name = Client._get(result,'file_name', file_name);
+                if (string_file_b64 != null){
+                    string_file_b64 = Client._get(result, 'file_b64') + string_file_b64;
+                }else{
+                    string_file_b64 = Client._get(result, 'file_b64');
+                }
+                if (file_id == null){
+                    callback(string_file_b64, result);
+                }else{
+                    download(file_id, callback);
+                }
+            });
+        }
+        download(file_id, function (string_file_b64, result) {
+            if (string_file_b64 == null){
+                callback_file(null);
+                console.error(result);
+            }else{
+                let file_bin = Buffer.from(string_file_b64, 'base64');
+                callback_file(file_bin);
+            }
+        });
+    }
+
+    storage_upload_file(bin, read_groups, write_groups, callback) {
+        let self = this;
+        function *div_chunks(text, n){
+            for (var i = 0; i < text.length; i+= n){
+                yield text.slice(i, i + n);
+            }
+        }
+        var buff = Buffer.from(bin, 'utf8');
+        let base64_data = buff.toString('base64');
+        var raw_base64 = Buffer.from(base64_data, 'utf8');
+        var base64_chunks = div_chunks(raw_base64, 1024 * 1024 * 4);
+        var parent_file_id = null;
+        let file_name = "file";
+        function upload(parent_file_id, base64_chunk, callback){
+            base64_chunk = base64_chunk.toString();
+            self.storage_upload_b64_chunk(parent_file_id, file_name, base64_chunk, read_groups, write_groups, function (data) {
+                var parent_file_id = data['file_id'];
+                var next_base64_chunk = base64_chunks.next();
+                if (next_base64_chunk.done){
+                    callback(data);
+                }else{
+                    upload(parent_file_id, next_base64_chunk.value, callback);
+                }
+            });
+        }
+        upload(parent_file_id, base64_chunks.next().value, callback);
+    }
+
+    log_create_log(event_source, event_name, event_param, callback) {
+        this._log('create_log', {
+            'event_source': event_source,
+            'event_name': event_name,
+            'event_param': event_param,
+        }, callback);
+    }
+
+    logic_run_function(function_name, payload, callback){
+        this._logic('logic', {
+            'function_name': function_name,
+            'payload': payload,
+        }, callback);
+    }
+
+}
